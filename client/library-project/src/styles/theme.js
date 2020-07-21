73a7b438bb69fb20bbda7d97737271032843018d
choijangho <jhchoi1115@gmail.com>
2020-07-21 19:39:11 +0900

    [#7] style: 기본 theme 설정 및 GlobalStyle 구성

---
+++ client/library-project/src/styles/theme.js
@@ -0,0 +1,22 @@
+const fontSizes = {
+  small: "1.5rem",
+  base: "1.6rem",
+  lg: "1.8rem",
+  xl: "2rem",
+  xxl: "2.2rem",
+  xxxl: "2.4rem",
+  titleSize: "3rem",
+};
+
+const colors = {
+  black: "#000000",
+  white: "#FFFFFF",
+  gray_1: "#222222",
+};
+
+const theme = {
+  fontSizes,
+  colors,
+};
+
+export default theme;
