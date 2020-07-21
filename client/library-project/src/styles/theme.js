de2da5be3b2596de06064fc83ebdce918fc084c2
choijangho <jhchoi1115@gmail.com>
2020-07-21 20:47:51 +0900

    [#7] style : media query device , deviceSizes 추가

--- client/library-project/src/styles/theme.js
+++ client/library-project/src/styles/theme.js
@@ -8,15 +8,33 @@ const fontSizes = {
   titleSize: "3rem",
 };
 
+const deviceSizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "450px",
+  tablet: "768px",
+  tabletL: "1024px",
+};
+
 const colors = {
   black: "#000000",
   white: "#FFFFFF",
   gray_1: "#222222",
 };
 
+const device = {
+  mobileS: `only screen and (max-width: ${deviceSizes.mobileS})`,
+  mobileM: `only screen and (max-width: ${deviceSizes.mobileM})`,
+  mobileL: `only screen and (max-width: ${deviceSizes.mobileL})`,
+  tablet: `only screen and (max-width: ${deviceSizes.tablet})`,
+  tabletL: `only screen and (max-width: ${deviceSizes.tabletL})`,
+};
+
 const theme = {
   fontSizes,
   colors,
+  deviceSizes,
+  device,
 };
 
 export default theme;
