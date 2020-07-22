d2efe9f27e1479ef577cb71a6b8f34509628670e
choijangho <jhchoi1115@gmail.com>
2020-07-23 03:05:21 +0900

    [#10] style : rem 계산 방식 변경 , paddings , interval theme 추가

--- client/library-project/src/styles/theme.js
+++ client/library-project/src/styles/theme.js
@@ -1,11 +1,29 @@
+const calcRem = (size) => `${size / 16}rem`;
+
 const fontSizes = {
-  small: "1.5rem",
-  base: "1.6rem",
-  lg: "1.8rem",
-  xl: "2rem",
-  xxl: "2.2rem",
-  xxxl: "2.4rem",
-  titleSize: "3rem",
+  small: calcRem(14),
+  base: calcRem(16),
+  lg: calcRem(18),
+  xl: calcRem(20),
+  xxl: calcRem(22),
+  xxxl: calcRem(24),
+  titleSize: calcRem(50),
+};
+
+const paddings = {
+  small: calcRem(8),
+  base: calcRem(10),
+  lg: calcRem(12),
+  xl: calcRem(14),
+  xxl: calcRem(16),
+  xxxl: calcRem(18),
+};
+
+const interval = {
+  base: calcRem(50),
+  lg: calcRem(100),
+  xl: calcRem(150),
+  xxl: calcRem(200),
 };
 
 const deviceSizes = {
@@ -20,6 +38,8 @@ const colors = {
   black: "#000000",
   white: "#FFFFFF",
   gray_1: "#222222",
+  gray_2: "#767676",
+  green_1: "#3cb46e",
 };
 
 const device = {
@@ -35,6 +55,8 @@ const theme = {
   colors,
   deviceSizes,
   device,
+  paddings,
+  interval,
 };
 
 export default theme;
