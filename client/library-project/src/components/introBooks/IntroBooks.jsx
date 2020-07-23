b4b536369acfab241771fbc766832ea9c192584e
choijangho <jhchoi1115@gmail.com>
2020-07-24 01:45:28 +0900

    [#13] feat : Mock Data를 BookCarousel Component로 전달하는 로직 추가

--- client/library-project/src/components/introBooks/IntroBooks.jsx
+++ client/library-project/src/components/introBooks/IntroBooks.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const introBooks = () => {
-  return <div></div>;
+import styled from "styled-components";
+
+import { introBooks } from "../../mock/mock";
+import BookCarousel from "./BookCarousel";
+
+const IntroBooks = () => {
+  const { iosBooks, frontEndBooks, backEndBooks, cultureBooks } = introBooks;
+
+  return (
+    <IntroBooksWrapper>
+      <BookCarousel {...iosBooks} />
+      <BookCarousel {...frontEndBooks} />
+      <BookCarousel {...backEndBooks} />
+      <BookCarousel {...cultureBooks} />
+    </IntroBooksWrapper>
+  );
 };
 
-export default introBooks;
+const IntroBooksWrapper = styled.div`
+  width: 100%;
+`;
+
+export default IntroBooks;
