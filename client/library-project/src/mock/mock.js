55f63e43b949f58f4997b1d6759e0cb48c429894
choijangho <jhchoi1115@gmail.com>
2020-07-24 01:20:23 +0900

    [#13] Feat : Intro Page Mock Data 구성

---
+++ client/library-project/src/mock/mock.js
@@ -0,0 +1,107 @@
+import sample_1 from "../assets/images/smaple-1.jpg";
+import sample_2 from "../assets/images/sample-2.jpg";
+import sample_3 from "../assets/images/sample-3.jpg";
+import sample_4 from "../assets/images/sample-4.jpg";
+
+export const introBooks = {
+  iosBooks: {
+    title: "IOS 관련 추천 서적",
+    bookList: [
+      {
+        bookTitle: "지금의 비행산업은 어떻게 형성됐나",
+        bookImage: sample_1,
+        writer: "",
+      },
+      {
+        bookTitle: "추악한 시대에도 아름다움을 본 당신",
+        bookImage: sample_1,
+        writer: "",
+      },
+      {
+        bookTitle: "피가 아닌 마음을 훔친 첫사랑",
+        bookImage: sample_1,
+        writer: "",
+      },
+      {
+        bookTitle: "사연은 다르지만 같은 곳의 우리",
+        bookImage: sample_1,
+        writer: "",
+      },
+    ],
+  },
+  frontEndBooks: {
+    title: "FE 관련 추천 서적",
+    bookList: [
+      {
+        bookTitle: "지금의 비행산업은 어떻게 형성됐나",
+        bookImage: sample_2,
+        writer: "",
+      },
+      {
+        bookTitle: "추악한 시대에도 아름다움을 본 당신",
+        bookImage: sample_2,
+        writer: "",
+      },
+      {
+        bookTitle: "피가 아닌 마음을 훔친 첫사랑",
+        bookImage: sample_2,
+        writer: "",
+      },
+      {
+        bookTitle: "사연은 다르지만 같은 곳의 우리",
+        bookImage: sample_2,
+        writer: "",
+      },
+    ],
+  },
+  backEndBooks: {
+    title: "BE 관련 추천 서적",
+    bookList: [
+      {
+        bookTitle: "지금의 비행산업은 어떻게 형성됐나",
+        bookImage: sample_3,
+        writer: "",
+      },
+      {
+        bookTitle: "추악한 시대에도 아름다움을 본 당신",
+        bookImage: sample_3,
+        writer: "",
+      },
+      {
+        bookTitle: "피가 아닌 마음을 훔친 첫사랑",
+        bookImage: sample_3,
+        writer: "",
+      },
+      {
+        bookTitle: "사연은 다르지만 같은 곳의 우리",
+        bookImage: sample_3,
+        writer: "",
+      },
+    ],
+  },
+  cultureBooks: {
+    title: "교양 관련 추천 서적",
+    bookList: [
+      {
+        bookTitle: "지금의 비행산업은 어떻게 형성됐나",
+        bookImage: sample_4,
+        writer: "",
+      },
+      {
+        bookTitle: "추악한 시대에도 아름다움을 본 당신",
+        bookImage: sample_4,
+        writer: "",
+      },
+      {
+        bookTitle: "피가 아닌 마음을 훔친 첫사랑",
+        bookImage: sample_4,
+        writer: "",
+      },
+      {
+        bookTitle: "사연은 다르지만 같은 곳의 우리",
+        bookImage: sample_4,
+        writer: "",
+      },
+    ],
+  },
+};
