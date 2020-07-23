20c57e8e6181dcb1b359dc82751d2b0c2d600234
choijangho <jhchoi1115@gmail.com>
2020-07-24 01:44:30 +0900

    [#13] Feat : Mock Data를 받아서 Rendering 해주는 로직 추가 및 기존 하드 코딩된 JSX 제거

--- client/library-project/src/components/introBooks/BookCarousel.jsx
+++ client/library-project/src/components/introBooks/BookCarousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Wrapper } from "../../styles/Wrapper";
-import sample_6 from "../../assets/images/sample-6.jpg";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -34,29 +33,23 @@ const CAROUSEL_SETTINGS = {
   ],
 };
 
-const BookCarousel = ({ carouselTitle, bookList }) => {
+const BookCarousel = (props) => {
+  const { carouselTitle, bookList } = props;
+
+  const carouselItems = bookList.map((el) => {
+    return (
+      <div className="carousel_item">
+        <img src={el.bookImage} alt="book cover" />
+        <div className="book_title">{el.bookTitle}</div>
+      </div>
+    );
+  });
+
   return (
     <Wrapper>
       <div className="inner">
-        <div className="title">IOS 관련 추천 서적</div>
-        <Slider {...CAROUSEL_SETTINGS}>
-          <div className="carousel_item">
-            <img src={sample_6} alt="" />
-            <div className="book_title">지금의 비행산업은 어떻게 형성됐나</div>
-          </div>
-          <div className="carousel_item">
-            <img src={sample_6} alt="" />
-            <div className="book_title">추악한 시대에도 아름다움을 본 당신</div>
-          </div>
-          <div className="carousel_item">
-            <img src={sample_6} alt="" />
-            <div className="book_title">피가 아닌 마음을 훔친 첫사랑</div>
-          </div>
-          <div className="carousel_item">
-            <img src={sample_6} alt="" />
-            <div className="book_title">사연은 다르지만 같은 곳의 우리</div>
-          </div>
-        </Slider>
+        <div className="title">{carouselTitle}</div>
+        <Slider {...CAROUSEL_SETTINGS}>{carouselItems}</Slider>
       </div>
     </Wrapper>
   );
