5433eba6f999fdce04bb1d1deb6776d510ac0f9c
choijangho <jhchoi1115@gmail.com>
2020-07-23 22:08:54 +0900

    [#10] Feat : App.js 에 Navbar , Search Component 적용

--- client/library-project/src/App.js
+++ client/library-project/src/App.js
@@ -3,11 +3,16 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 
+import Navbar from "./components/navbar/Navbar";
+import Search from "./components/search/Search";
+
 const App = () => {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Navbar />
+        <Search />
       </ThemeProvider>
     </div>
   );
