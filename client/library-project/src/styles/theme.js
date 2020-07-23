1d4b8cace2a55956ab11781f0ef655b3da530489
choijangho <jhchoi1115@gmail.com>
2020-07-24 01:23:01 +0900

    [#13] style : margins , gradient theme 추가

--- client/library-project/src/styles/theme.js
+++ client/library-project/src/styles/theme.js
@@ -19,6 +19,15 @@ const paddings = {
   xxxl: calcRem(18),
 };
 
+const margins = {
+  small: calcRem(8),
+  base: calcRem(10),
+  lg: calcRem(12),
+  xl: calcRem(14),
+  xxl: calcRem(16),
+  xxxl: calcRem(18),
+};
+
 const interval = {
   base: calcRem(50),
   lg: calcRem(100),
@@ -54,14 +63,21 @@ const device = {
   tabletL: `only screen and (max-width: ${deviceSizes.tabletL})`,
 };
 
+const gradient = {
+  gradient_1:
+    "linear-gradient(180deg,rgba(0,0,0,.38) 0,rgba(0,0,0,.38) 3.5%,rgba(0,0,0,.379) 7%,rgba(0,0,0,.377) 10.35%,rgba(0,0,0,.375) 13.85%,rgba(0,0,0,.372) 17.35%,rgba(0,0,0,.369) 20.85%,rgba(0,0,0,.366) 24.35%,rgba(0,0,0,.364) 27.85%,rgba(0,0,0,.361) 31.35%,rgba(0,0,0,.358) 34.85%,rgba(0,0,0,.355) 38.35%,rgba(0,0,0,.353) 41.85%,rgba(0,0,0,.351) 45.35%,rgba(0,0,0,.35) 48.85%,rgba(0,0,0,.353) 52.35%,rgba(0,0,0,.36) 55.85%,rgba(0,0,0,.371) 59.35%,rgba(0,0,0,.385) 62.85%,rgba(0,0,0,.402) 66.35%,rgba(0,0,0,.42) 69.85%,rgba(0,0,0,.44) 73.35%,rgba(0,0,0,.46) 76.85%,rgba(0,0,0,.48) 80.35%,rgba(0,0,0,.498) 83.85%,rgba(0,0,0,.515) 87.35%,rgba(0,0,0,.529) 90.85%,rgba(0,0,0,.54) 94.35%,rgba(0,0,0,.547) 97.85%,rgba(0,0,0,.55));",
+};
+
 const theme = {
   fontSizes,
   colors,
   deviceSizes,
   device,
   paddings,
+  margins,
   interval,
   verticalInterval,
+  gradient,
 };
 
 export default theme;
