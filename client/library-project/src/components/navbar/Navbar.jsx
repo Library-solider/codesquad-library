4897581195a9614293243e43c8ef6f6154eadad8
choijangho <jhchoi1115@gmail.com>
2020-07-24 11:54:10 +0900

    [#13] docs : withTheme -> ThemeContext로 변경

--- client/library-project/src/components/navbar/Navbar.jsx
+++ client/library-project/src/components/navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import styled, { withTheme } from "styled-components";
+import styled, { ThemeContext } from "styled-components";
 import { Button } from "../../styles/Button";
 
-const Navbar = (props) => {
-  const { theme } = props;
+const Navbar = () => {
+  const themeContext = useContext(ThemeContext);
 
   return (
     <NavbarWrapper>
       <Logo></Logo>
-      <Button fillColor={theme.colors.green_1} textColor={theme.colors.white}>
+      <Button
+        fillColor={themeContext.colors.green_1}
+        textColor={themeContext.colors.white}
+      >
         Login
       </Button>
     </NavbarWrapper>
@@ -28,4 +31,4 @@ const Logo = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.xl};
 `;
 
-export default withTheme(Navbar);
+export default Navbar;
