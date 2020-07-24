4da46f3a5ce1e4e2dcadd975b911f67fd191380d
choijangho <jhchoi1115@gmail.com>
2020-07-24 11:55:15 +0900

    [#13] feat : Search Button , icon 추가 및 Style 수정

--- client/library-project/src/components/search/Search.jsx
+++ client/library-project/src/components/search/Search.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import book from "../../assets/images/book-image.jpg";
+import { FiSearch } from "react-icons/fi";
+
+const MAIN_TITLE = "CODE SQUAD LIBEARY";
+const INTRODUCTION = "각 언어별 서적 및 다양한 종류의 서적이 준비돼 있습니다.";
 
 const Search = () => {
   return (
@@ -8,15 +12,17 @@ const Search = () => {
       <img src={book} alt="search background" />
       <SearchInner>
         <div>
-          <div className="title interval">CODE SQUAD LIBEARY</div>
-          <div className="introduction interval">
-            각 언어별 서적 및 다양한 종류의 서적이 준비돼 있습니다.
-          </div>
-          <div>
+          <div className="title interval">{MAIN_TITLE}</div>
+          <div className="introduction interval">{INTRODUCTION}</div>
+          <div className="search_form">
             <input
               type="text"
               placeholder="원하는 책의 제목을 검색해 보세요 !"
             />
+            <button className="search_btn">
+              <FiSearch />
+            </button>
+            <div className="search_result"></div>
           </div>
         </div>
       </SearchInner>
@@ -45,12 +51,29 @@ const SearchInner = styled.div`
   background: ${({ theme: { gradient } }) => gradient.gradient_1};
 
   input {
-    width: 50vw;
-    border-radius: 4px;
+    width: inherit;
     padding: ${({ theme: { paddings } }) => paddings.lg};
+    border-top-left-radius: ${({ theme: { radius } }) => radius.xSmall};
+    border-bottom-left-radius: ${({ theme: { radius } }) => radius.xSmall};
   }
 
-  img {
+  .search_form {
+    display: flex;
+    width: 50vw;
+  }
+
+  .search_btn {
+    padding-right: ${({ theme: { paddings } }) => paddings.small};
+    background-color: ${({ theme: { colors } }) => colors.white};
+    border-top-right-radius: ${({ theme: { radius } }) => radius.xSmall};
+    border-bottom-right-radius: ${({ theme: { radius } }) => radius.xSmall};
+
+    svg {
+      font-size: ${({ theme: { fontSizes } }) => fontSizes.xxxl};
+    }
+    :hover {
+      color: ${({ theme: { colors } }) => colors.blue};
+    }
   }
 
   .title {
