14d9c93f6fcb9ddc3115cfe1965bd72284daa5a0
choijangho <jhchoi1115@gmail.com>
2020-07-28 15:54:07 +0900

    [#17] feat : Navbar에 검색창 추가

--- client/library-project/src/components/navbar/Navbar.jsx
+++ client/library-project/src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 
 import styled, { ThemeContext } from "styled-components";
+import codesquad_logo from "../../assets/images/codesquad-logo.png";
+import Search from "../search/Search";
 import { Button } from "../../styles/Button";
 
 const Navbar = () => {
@@ -8,13 +10,17 @@ const Navbar = () => {
 
   return (
     <NavbarWrapper>
-      <Logo></Logo>
-      <Button
+      <Logo>
+        <img src={codesquad_logo} alt="codesquad logo" />
+      </Logo>
+      <Search />
+
+      <LoginButton
         fillColor={themeContext.colors.green_1}
         textColor={themeContext.colors.white}
       >
         Login
-      </Button>
+      </LoginButton>
     </NavbarWrapper>
   );
 };
@@ -23,12 +29,22 @@ const NavbarWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
+
   padding: ${({ theme }) => theme.paddings.xl};
   font-size: ${({ theme }) => theme.fontSizes.base};
   background-color: ${({ theme: { colors } }) => colors.gray_1};
 `;
 
+const LoginButton = styled(Button)`
+  order: 1;
+`;
+
 const Logo = styled.div`
+  img {
+    width: 4.5rem;
+    height: 3.5rem;
+  }
   font-size: ${({ theme }) => theme.fontSizes.xl};
 `;
 
