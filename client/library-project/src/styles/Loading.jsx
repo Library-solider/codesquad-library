3c407e9f32997aefedd63739083e503d6bf336cc
choijangho <jhchoi1115@gmail.com>
2020-08-10 23:48:49 +0900

    [#23] style : Loading Style 추가

---
+++ client/library-project/src/styles/Loading.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 50vw;
+  img {
+    width: 300px;
+    height: auto;
+  }
+`;
