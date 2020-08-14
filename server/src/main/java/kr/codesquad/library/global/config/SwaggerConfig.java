4e1e5ca7fcd57f0fec5a309e503632ed6afd742b
Sunny <thstkd92@gmail.com>
2020-08-14 16:46:42 +0900

    [#62] fix: Swagger설정이 Cors에러를 발생하여 삭제함.

--- server/src/main/java/kr/codesquad/library/global/config/SwaggerConfig.java
+++ server/src/main/java/kr/codesquad/library/global/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package kr.codesquad.library.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,15 +18,6 @@ import java.util.ArrayList;
 public class SwaggerConfig extends WebMvcConfigurationSupport {
     private String title;
     private String contactUrl;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
     @Bean
     public Docket api() {
