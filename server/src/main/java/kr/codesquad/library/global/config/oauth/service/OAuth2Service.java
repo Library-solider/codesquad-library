6877778db125afaf4576af5438b2d25cb2866e62
Sunny <thstkd92@gmail.com>
2020-08-14 13:32:34 +0900

    [#52] feat: Account 세션 추가.
    - SessionAccount: 직렬화(Serializable) 구현. Account Entity는 언제든지 변할 가능성이 있어 SessionDto 생성.
    
    - OAuth2Service: 세션 생성하기 위한 HttpSession 추가

--- server/src/main/java/kr/codesquad/library/global/config/oauth/service/OAuth2Service.java
+++ server/src/main/java/kr/codesquad/library/global/config/oauth/service/OAuth2Service.java
@@ -3,12 +3,14 @@ package kr.codesquad.library.global.config.oauth.service;
 import kr.codesquad.library.domain.account.Account;
 import kr.codesquad.library.domain.account.AccountRepository;
 import kr.codesquad.library.global.config.oauth.dto.OAuthAttributes;
+import kr.codesquad.library.global.config.oauth.dto.SessionAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.Map;
 
@@ -17,10 +19,12 @@ import java.util.Map;
 public class OAuth2Service {
 
     private final AccountRepository accountRepository;
+    private final HttpSession httpSession;
 
     protected OAuth2User getUser(String userNameAttributeName, Map<String, Object> attributesMap) {
         OAuthAttributes attributes = OAuthAttributes.of(userNameAttributeName, attributesMap);
         Account account = saveOrUpdate(attributes);
+        httpSession.setAttribute("account", new SessionAccount(account));
 
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(account.getRoleKey())),
