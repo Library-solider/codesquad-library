62b0dbab1984f160477646150bb7a48f67ffb366
Sunny <thstkd92@gmail.com>
2020-08-18 14:00:17 +0900

    [#71] fix: 디렉토리 재정리

--- server/src/main/java/kr/codesquad/library/domain/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.codesquad.library.domain.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
