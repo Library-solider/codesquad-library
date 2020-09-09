98a7c369deddafbe83c0847b3b874795a4ad39cd
Sunny <thstkd92@gmail.com>
2020-09-09 17:20:28 +0900

    [#95] fix: 로그인 프로필 500에러 수정 및 JWT 삭제
    - AccountController
      - handleAccountException(): 로그아웃일 때 에러처리 추가.
    
    - OAuthAttributes: OAuth 리소스 서버가 여러개 일 경우 대비 변경.
    - ProviderAttributes: OAuth 리소스 서버 enum 클래스. 현재는 Github만 추가함.
    
    - SecurityConfig: 성공시 리다이렉트 주소 환경변수로 설정.

--- src/main/java/kr/codesquad/library/global/config/oauth/dto/AccountPrincipal.java
+++ src/main/java/kr/codesquad/library/global/config/oauth/dto/AccountPrincipal.java
@@ -1,16 +1,12 @@
 package kr.codesquad.library.global.config.oauth.dto;
 
-import kr.codesquad.library.domain.account.Account;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 public class AccountPrincipal implements OAuth2User {
@@ -20,12 +16,12 @@ public class AccountPrincipal implements OAuth2User {
     private final String name;
     private final String email;
     private final String avatarUrl;
-    private final Collection<? extends GrantedAuthority> authorities;
+    private final Set<GrantedAuthority> authorities;
     private final Map<String, Object> attributes;
 
     @Builder
     private AccountPrincipal(Long id, Long oauthId, String name, String email, String avatarUrl,
-                             Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes) {
+                             Set<GrantedAuthority> authorities, Map<String, Object> attributes) {
         this.id = id;
         this.oauthId = oauthId;
         this.name = name;
@@ -35,28 +31,13 @@ public class AccountPrincipal implements OAuth2User {
         this.attributes = attributes;
     }
 
-    public static AccountPrincipal create(Account account, Map<String, Object> attributes) {
-        List<GrantedAuthority> authorities = Collections.singletonList(
-                new SimpleGrantedAuthority(account.getRoleKey()));
-
-        return AccountPrincipal.builder()
-                .id(account.getId())
-                .oauthId(account.getOauthId())
-                .name(account.getName())
-                .email(account.getEmail())
-                .avatarUrl(account.getAvatarUrl())
-                .authorities(authorities)
-                .attributes(attributes)
-                .build();
-    }
-
     @Override
     public String getName() {
         return name;
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+    public Set<GrantedAuthority> getAuthorities() {
         return this.authorities;
     }
 
--- src/main/java/kr/codesquad/library/global/config/oauth/security/CustomOAuth2UserService.java
+++ src/main/java/kr/codesquad/library/global/config/oauth/security/CustomOAuth2UserService.java
@@ -3,15 +3,19 @@ package kr.codesquad.library.global.config.oauth.security;
 import kr.codesquad.library.domain.account.Account;
 import kr.codesquad.library.domain.account.AccountRepository;
 import kr.codesquad.library.domain.account.LibraryRole;
-import kr.codesquad.library.global.config.oauth.dto.GithubOAuth2UserInfo;
+import kr.codesquad.library.global.config.oauth.dto.OAuthAttributes;
 import kr.codesquad.library.global.config.oauth.dto.AccountPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -21,20 +25,33 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        return processOAuth2User(oAuth2User);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        return processOAuth2User(registrationId, userNameAttributeName, oAuth2User.getAttributes());
     }
 
-    private OAuth2User processOAuth2User(OAuth2User oAuth2User) {
-        GithubOAuth2UserInfo userInfo = GithubOAuth2UserInfo.of(oAuth2User.getAttributes());
+    private OAuth2User processOAuth2User(String registrationId, String userNameAttributeName,
+                                         Map<String, Object> attributes) {
+        OAuthAttributes userInfo = OAuthAttributes.of(registrationId, userNameAttributeName, attributes);
         Account account = saveOrUpdate(userInfo);
 
-        return AccountPrincipal.create(account, userInfo.getAttributes());
+        return AccountPrincipal.builder()
+                .id(account.getId())
+                .oauthId(account.getOauthId())
+                .name(account.getName())
+                .email(account.getEmail())
+                .avatarUrl(account.getAvatarUrl())
+                .authorities(Collections.singleton(new SimpleGrantedAuthority(account.getRoleKey())))
+                .attributes(attributes)
+                .build();
     }
 
-    private Account saveOrUpdate(GithubOAuth2UserInfo userInfo) {
+    private Account saveOrUpdate(OAuthAttributes userInfo) {
         Account account = accountRepository.findByOauthId(userInfo.getOauthId())
                 .map(entity -> entity.update(userInfo.getName(), userInfo.getAvatarUrl()))
                 .orElse(registerNewAccount(userInfo));
@@ -42,7 +59,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         return accountRepository.save(account);
     }
 
-    private Account registerNewAccount(GithubOAuth2UserInfo userInfo) {
+    private Account registerNewAccount(OAuthAttributes userInfo) {
         return Account.builder()
                 .oauthId(userInfo.getOauthId())
                 .name(userInfo.getName())
