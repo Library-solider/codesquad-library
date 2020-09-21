ad1e0d4608fa50579c27f3f45165b44dfae30062
ragdoll <museopkim0214@gmail.com>
2020-09-21 16:57:21 +0900

    [#109] fix: admin 관련 매핑 URL 변경
    - AccountAdminController
       - REST API로 JSON 통신을 하지 않고 서버 사이드에서 뷰를 렌더링 하기에 /v1/ 생략
       - "/v1/admin/**" -> "/admin/**" 변경
    
    - 통일한 코딩 컨벤션에 맞춰 Lombok 애노테이션을 최상단으로 이동

--- src/main/java/kr/codesquad/library/admin/controller/AccountAdminController.java
+++ src/main/java/kr/codesquad/library/admin/controller/AccountAdminController.java
@@ -1,6 +1,6 @@
 package kr.codesquad.library.admin.controller;
 
-import kr.codesquad.library.admin.model.account.AccountSummary;
+import kr.codesquad.library.admin.domain.account.AccountSummaryResponse;
 import kr.codesquad.library.admin.service.AccountAdminService;
 import kr.codesquad.library.domain.account.LibraryRole;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller
 @RequiredArgsConstructor
-@RequestMapping("/v1/admin")
+@Controller
+@RequestMapping("/admin")
 public class AccountAdminController {
 
     private final AccountAdminService accountAdminService;
@@ -25,7 +25,7 @@ public class AccountAdminController {
 
     @GetMapping("/main")
     public String main(Model model) {
-        List<AccountSummary> accountSummaries = accountAdminService.findAllAccountsByRole(LibraryRole.GUEST);
+        List<AccountSummaryResponse> accountSummaries = accountAdminService.findAllAccountsByRole(LibraryRole.GUEST);
         model.addAttribute("accountSummaries", accountSummaries);
         return "index";
     }
