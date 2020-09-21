61c83e188dd2b6e4418f9f8b5e5dd414cc82ded9
ragdoll <museopkim0214@gmail.com>
2020-09-21 18:11:03 +0900

    [#109] fix: JPA Repository에서 @Repository 제거
    - AccountAdminRepository에서 @Repository 제거

--- src/main/java/kr/codesquad/library/admin/domain/account/AccountAdminRepository.java
+++ src/main/java/kr/codesquad/library/admin/domain/account/AccountAdminRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface AccountAdminRepository extends JpaRepository<Account, Long> {
 
     List<Account> findAllByLibraryRole(LibraryRole libraryRole);
