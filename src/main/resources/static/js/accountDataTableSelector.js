ec0f366b810125aade226e5d2b7ce6545f0e78ba
ragdoll <museopkim0214@gmail.com>
2020-09-29 02:17:08 +0900

    [#122] feat: 회원 데이터 테이블 hover 효과 및 데이터 접근 기능 적용
    js/accountDataTableSelector.js
    - 회원의 목록에 마우스 오버 했을 때 hover 효과가 적용 되도록 적용
    - 해당 리스트에서 특정 회원 데이터를 클릭하면 해당 상세정보로 이동
       - addStylesOnData() : 마우스 오버 했을때 색상 효과 및 포인터 적용
       - removeStylesOnData() : 마우스 포인터를 바깥으로 이동 했을 때 효과 삭제
       - getDataId() : 마우스 클릭 시 해당 데이터의 ID 값을 찾아서 URL 이동
       - main() : 마우스 오버, 마우스 아웃, 마우스 클릭 이벤트 등록 및 실행
    
    templates/account/all-accounts.html
    - JS SRC 추가
    - JS에서 DOM 선택 및 활용의 편의성을 위해 태그에 jsData, jsDataId 클래스 추가

---
+++ src/main/resources/static/js/accountDataTableSelector.js
@@ -0,0 +1,29 @@
+function addStylesOnData(event) {
+    const data = event.currentTarget.parentNode;
+    const dataNodes = data.querySelectorAll('td');
+    dataNodes.forEach(dataNode => dataNode.classList.add('data-table__row--hover'));
+}
+
+function removeStylesOnData(event) {
+    const data = event.currentTarget.parentNode;
+    const dataNodes = data.querySelectorAll('td');
+    dataNodes.forEach(dataNode => dataNode.classList.remove('data-table__row--hover'));
+}
+
+function getDataId(event) {
+    const data = event.currentTarget.parentNode;
+    const dataId = data.querySelector('.jsDataId').innerHTML;
+    console.log(dataId);
+    window.location.href = `/admin/users/${dataId}`;
+}
+
+function main() {
+    const dataList = document.querySelectorAll('.jsData');
+    dataList.forEach(data => {
+        data.addEventListener('mouseover', addStylesOnData);
+        data.addEventListener('mouseout', removeStylesOnData);
+        data.addEventListener('click', getDataId)
+    });
+}
+
+main();
