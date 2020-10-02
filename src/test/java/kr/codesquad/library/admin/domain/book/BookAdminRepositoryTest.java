0dd498380a706b0f1ad966a42609ee7266b00912
ragdoll <museopkim0214@gmail.com>
2020-10-03 03:29:21 +0900

    [#133] fix: 도서 목록 조회 Repository Test 수정
    BookAdminRepositoryTest
    - 페이징 기능이 추가 됨에 따라 변경
    - page, pageSize 검증

--- src/test/java/kr/codesquad/library/admin/domain/book/BookAdminRepositoryTest.java
+++ src/test/java/kr/codesquad/library/admin/domain/book/BookAdminRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,20 +21,21 @@ class BookAdminRepositoryTest {
     @Autowired
     BookAdminRepository bookAdminRepository;
 
-    @CsvSource({"431, 0, 1, 컴퓨터공학/IT, 9791185890678"})
+    @CsvSource({"1, 10, 0, 1, 컴퓨터공학/IT, 9791185890678"})
     @ParameterizedTest
-    public void 모든_도서정보를_카테고리와_함께_조회한다(int totalBookCount, int index, Long bookId,
-                                                String categoryTitle, String isbn) {
+    public void 모든_도서정보를_카테고리와_함께_조회한다(int page, int pageSize, int index,
+                                                Long bookId, String categoryTitle, String isbn) {
         //when
-        List<Book> books = bookAdminRepository.findAllWithCategory();
-        Book selectedBook = books.stream()
-                                 .filter(book -> book.getId().equals(bookId))
-                                 .collect(Collectors.toList())
-                                 .get(index);
+        Page<Book> books = bookAdminRepository.findAllWithCategory(PageRequest.of(page - 1, pageSize));
+        List<Book> bookEntities = books.getContent();
+        Book selectedBook = bookEntities.stream()
+                                        .filter(book -> book.getId().equals(bookId))
+                                        .collect(Collectors.toList())
+                                        .get(index);
 
         //then
         assertAll(
-                () -> assertThat(books.size()).isEqualTo(totalBookCount),
+                () -> assertThat(books.getSize()).isEqualTo(pageSize),
                 () -> assertThat(selectedBook.getId()).isEqualTo(bookId),
                 () -> assertThat(selectedBook.getCategory().getTitle()).isEqualTo(categoryTitle),
                 () -> assertThat(selectedBook.getIsbn()).isEqualTo(isbn)
