de07938787c735155e4123c48db257b56454efd6
ragdoll <museopkim0214@gmail.com>
2020-10-03 02:07:17 +0900

    [#133] refactor: BookSummary 정적 메서드 코드 라인 정리
    BookSummary
    - 빌더 메서드 라인 정리

--- src/main/java/kr/codesquad/library/admin/domain/book/BookSummary.java
+++ src/main/java/kr/codesquad/library/admin/domain/book/BookSummary.java
@@ -24,11 +24,11 @@ public class BookSummary {
 
     public static BookSummary from(Book book) {
         return BookSummary.builder()
-                                  .id(book.getId())
-                                  .title(book.getTitle())
-                                  .author(book.getAuthor())
-                                  .categoryTitle(book.getCategory().getTitle())
-                                  .isbn(book.getIsbn())
-                                  .build();
+                          .id(book.getId())
+                          .title(book.getTitle())
+                          .author(book.getAuthor())
+                          .categoryTitle(book.getCategory().getTitle())
+                          .isbn(book.getIsbn())
+                          .build();
     }
 }
