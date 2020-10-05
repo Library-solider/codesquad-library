46f898b2da3022f8247548d17675a3f2f0a2f0ff
ragdoll <museopkim0214@gmail.com>
2020-10-06 03:07:20 +0900

    [#135] fix: 도서 수정 기능 구현을 위한 BookData 클래스에 정적 메서드 추가
    BookData
    - from() : 도서 수정에 요구되는 도서 데이터는 Open API의 데이터를 바인딩하는 클래스 구성 요소와 정확히 일치. 따라서 기존 BookData 클래스에 정적 메서드를 추가하여 외부에서 생성할 수 있게 수정

--- src/main/java/kr/codesquad/library/admin/domain/bookopenapi/BookData.java
+++ src/main/java/kr/codesquad/library/admin/domain/bookopenapi/BookData.java
@@ -2,10 +2,12 @@ package kr.codesquad.library.admin.domain.bookopenapi;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import kr.codesquad.library.domain.book.Book;
 import lombok.*;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
 @Getter
 public class BookData {
 
@@ -21,4 +23,28 @@ public class BookData {
     @JsonProperty("pubDate")
     @JsonFormat(pattern = "yyyyMMdd")
     private LocalDate publicationDate;
+
+    @Builder
+    private BookData(String title, String description, String author, String publisher,
+                     String isbn, String imageUrl, LocalDate publicationDate) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.imageUrl = imageUrl;
+        this.publicationDate = publicationDate;
+    }
+
+    public static BookData from(Book book) {
+        return BookData.builder()
+                       .title(book.getTitle())
+                       .description(book.getDescription())
+                       .author(book.getAuthor())
+                       .publisher(book.getPublisher())
+                       .isbn(book.getIsbn())
+                       .imageUrl(book.getImageUrl())
+                       .publicationDate(book.getPublicationDate())
+                       .build();
+    }
 }
