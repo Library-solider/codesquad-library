fb9e54e6754ccb0f70d4cdd2d16471059941d213
ragdoll <museopkim0214@gmail.com>
2020-10-05 20:40:35 +0900

    [#134] feat: 도서 목록 테이블 -> 상세 정보 이동 로직 JS 코드 구현
    js/bookDataTableHandler.js
    - addStylesOnData() : hover -> hover효과 CSS 클래스 적용
    - removeStylesFromData() : out -> hover효과 CSS 클래스 제거
    - getDataId() : click -> 해당 ID의 도서 상세정보로 이동
    
    css/main.css
    - 도서 테이블에서도 회원 테이블과 동일한 hover효과를 적용하기 위해 .data-table의 자식 클래스에만 hover 클래스가 적용 되도록 한정 하던 것을 제거

---
+++ src/main/resources/static/js/bookDataTableHandler.js
@@ -0,0 +1,30 @@
+function addStylesOnData(event) {
+    const data = event.currentTarget.parentNode;
+    const dataNodes = data.querySelectorAll('td');
+    console.log(dataNodes);
+    dataNodes.forEach(dataNode => dataNode.classList.add('data-table__row--hover'));
+}
+
+function removeStylesFromData(event) {
+    const data = event.currentTarget.parentNode;
+    const dataNodes = data.querySelectorAll('td');
+    dataNodes.forEach(dataNode => dataNode.classList.remove('data-table__row--hover'));
+}
+
+function getDataId(event) {
+    const data = event.currentTarget.parentNode;
+    const dataId = data.querySelector('.jsDataId').innerHTML;
+    console.log(dataId);
+    window.location.href = `/admin/books/${dataId}`;
+}
+
+function main() {
+    const dataList = document.querySelectorAll('.jsData');
+    dataList.forEach(data => {
+        data.addEventListener('mouseover', addStylesOnData);
+        data.addEventListener('mouseout', removeStylesFromData);
+        data.addEventListener('click', getDataId)
+    });
+}
+
+main();
