9633d9f8b296bdc2f00fc8e571ebbf92a43ed4b4
ragdoll <museopkim0214@gmail.com>
2020-10-07 04:19:53 +0900

    [#143] fix: AuthAdminController에 @ReuqestMapping 누락되어 있던 것을 수정
    AuthAdminController
    - @RequestMapping("/admin") 추가

--- src/main/java/kr/codesquad/library/admin/controller/AuthAdminController.java
+++ src/main/java/kr/codesquad/library/admin/controller/AuthAdminController.java
@@ -1,11 +1,17 @@
 package kr.codesquad.library.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/admin")
 public class AuthAdminController {
 
     @GetMapping("/login")
