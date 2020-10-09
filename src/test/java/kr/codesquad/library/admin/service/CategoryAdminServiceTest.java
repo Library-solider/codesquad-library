334a771160f0db2393baed5c917bfca10a72eed1
ragdoll <museopkim0214@gmail.com>
2020-10-10 00:28:22 +0900

    [#147] test: 카테고리 목록 조회 테스트에 book count 관련 테스트 추가
    CategoryAdminServiceTest
    - 비즈니스 로직이 변경됨에 따라 추가

--- src/test/java/kr/codesquad/library/admin/service/CategoryAdminServiceTest.java
+++ src/test/java/kr/codesquad/library/admin/service/CategoryAdminServiceTest.java
@@ -1,6 +1,6 @@
 package kr.codesquad.library.admin.service;
 
-import kr.codesquad.library.admin.domain.category.CategoryDetailResponse;
+import kr.codesquad.library.admin.domain.category.CategoryDetail;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +16,19 @@ class CategoryAdminServiceTest {
     @Autowired
     CategoryAdminService categoryAdminService;
 
-    @CsvSource({"5"})
+    @CsvSource({"5, 3, 64"})
     @ParameterizedTest
-    public void 모든_카테고리를_조회한다(int categorySize) {
+    public void 모든_카테고리를_조회한다(int categoryCount, Long categoryId, int bookCount) {
         //when
-        List<CategoryDetailResponse> categories = categoryAdminService.findAllCategory();
+        List<CategoryDetail> categories = categoryAdminService.findAllCategory();
+        CategoryDetail categoryDetail = categories.stream()
+                                                  .filter(category -> category.getId().equals(categoryId))
+                                                  .findAny()
+                                                  .orElse(null);
 
         //then
-        assertThat(categories.size()).isEqualTo(categorySize);
+        assertThat(categories.size()).isEqualTo(categoryCount);
+        assertThat(categoryDetail.getBookCount()).isEqualTo(bookCount);
     }
 
 }
