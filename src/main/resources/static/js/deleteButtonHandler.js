3c148779de31a5465f01efd6a1903ecba902e0c3
ragdoll <museopkim0214@gmail.com>
2020-10-11 02:22:01 +0900

    [#149] feat: 카테고리 삭제 버튼 핸들러 JS 추가
    js/deleteButtonHandler
    - delete 문자열을 입력해서 일치할 때만 삭제를 진행
    
    templates/categories-books
    - 버튼 핸들러 JS 코드 추가

---
+++ src/main/resources/static/js/deleteButtonHandler.js
@@ -0,0 +1,28 @@
+function validateDeleteRequest(event, validationWord, deleteForm) {
+    event.preventDefault();
+    swal({
+            title: "삭제 확인",
+            text: "삭제를 진행 하려면 'delete' 를 정확히 입력 해주세요.",
+            type: "input",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputPlaceholder: "delete"
+        },
+        function(inputValue){
+            if (inputValue === validationWord) {
+               deleteForm.submit();
+               return;
+            }
+            swal.showInputError("입력 값이 다릅니다. 다시 시도 해주세요!");
+        });
+}
+
+function main() {
+    const deleteForm = document.querySelector('.jsDeleteForm');
+    const deleteButton = document.querySelector('.jsDeleteButton');
+    const validationWord = 'delete';
+    deleteButton.addEventListener('click', (event) =>  validateDeleteRequest(event, validationWord, deleteForm));
+}
+
+main();
