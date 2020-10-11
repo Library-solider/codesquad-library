3070a8cf94fdfcd98f2ddb9fbd6c46ae2fbd3b4f
ragdoll <museopkim0214@gmail.com>
2020-10-11 17:42:40 +0900

    [#149] test: 도서 이동 기능 테스트 코드 작성
    BookAdminServiceTest
    - ParameterizedTest에 bookId 컬렉션을 사용하기 위해 MethodSource를 활용
    
    BookMoveRequest
    - 테스트 코드 작성을 위해 생성자 및 팩토리 메서드 추가 (DTO로만 사용을 할 때는 Getter만 있어도 충분한데, 테스트 코드 작성 때문에 생성자를 만들어야 하는지 공부가 필요)

--- src/main/java/kr/codesquad/library/admin/domain/book/request/BookMoveRequest.java
+++ src/main/java/kr/codesquad/library/admin/domain/book/request/BookMoveRequest.java
@@ -10,4 +10,14 @@ public class BookMoveRequest {
     private List<Long> bookIds;
     private Long categoryId;
     private Long bookcaseId;
+
+    private BookMoveRequest(List<Long> bookIds, Long categoryId, Long bookcaseId) {
+        this.bookIds = bookIds;
+        this.categoryId = categoryId;
+        this.bookcaseId = bookcaseId;
+    }
+
+    public static BookMoveRequest of(List<Long> bookIds, Long categoryId, Long bookcaseId) {
+        return new BookMoveRequest(bookIds, categoryId, bookcaseId);
+    }
 }
